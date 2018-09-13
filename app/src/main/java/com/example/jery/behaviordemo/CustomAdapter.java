75839944bdbc5db4c054ba6cb330b369ec9c0f79
zhangjian1 <zhangjian1@yixia.com>
2018-09-13 18:04:08 +0800

    update

--- app/src/main/java/com/example/jery/behaviordemo/CustomAdapter.java
+++ app/src/main/java/com/example/jery/behaviordemo/CustomAdapter.java
@@ -4,8 +4,19 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomAdapterViewHolder> {
+    private List<String> list = new ArrayList<>();
+
+    public CustomAdapter() {
+        for (int i = 0; i < 20; i++) {
+            list.add("item" + i);
+        }
+    }
 
     @NonNull
     @Override
@@ -15,18 +26,20 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomAdap
 
     @Override
     public void onBindViewHolder(@NonNull CustomAdapterViewHolder holder, int position) {
-
+        holder.tv.setText(list.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return list.size();
     }
 
     class CustomAdapterViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
 
         public CustomAdapterViewHolder(View itemView) {
             super(itemView);
+            tv = itemView.findViewById(R.id.tv);
         }
     }
 }
--- app/src/main/java/com/example/jery/behaviordemo/MainActivity.java
+++ app/src/main/java/com/example/jery/behaviordemo/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.jery.behaviordemo;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -16,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         recyclerView = findViewById(R.id.my_list);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
